4e3534da7b6c2d25305885cc114d767d7eb39e7d
Engine Bai <enginebai@gmail.com>
2023-05-01 22:33:56 +0800

    Add keywords and its similar terms

--- prompts_parser.py
+++ prompts_parser.py
@@ -10,11 +10,12 @@ question_dir = 'question'
 def parse_all_question_files():
     question_files = list_question_files()
     result_map = dict()
+    print("Parsing files...")
     for f_ in question_files:
-        print(f"Paring {f_}")
         result_map.update(read_file(f_))
     result_map = dict(sorted(result_map.items(), key=lambda x: len(x[1])))
-    print_key_values(result_map)
+    print_keys(result_map)
+    # print_key_values(result_map)
 
 
 def list_question_files():
@@ -65,5 +66,24 @@ def print_key_values(map_to_print: dict):
                 f.write(f'\t{terms}\n')
 
 
+def print_keys(map_to_print: dict):
+    for k in map_to_print.keys():
+        print(f'{k} ({len(map_to_print[k])})')
+
+
 parse_all_question_files()
 # read_file('question/332.md')
+
+# TODO: Add more keywords for each category
+keywords = {'Workout': {'健身', },
+            'Travel': {'旅遊', '旅游', '旅行', '出行', '出游', '出差', '旅途', '旅程', '行程', '行程安排', '行程规划',
+                       '行程计划', '行程安排', '行程规划', '行程计划', '行程安排', '行程规划', '行程计划', '行程安排',
+                       '行程规划', '行程计划', '行程安排', '行程规划', '行程计划', '行程安排', '行程规划', '行程计划',
+                       '行程安排', '行程规划', '行程计划', '行程安排', '行程规划', '行程计划', },
+            'Food': {'美食', },
+            'Learning': {'学习', '学習', '學習', '學习', '学习计划', '学習計劃', '學習計劃', '学習計劃', '学习计划',
+                         '学習計劃', '學習計劃', '学習計劃', '学习计划', '学習計劃', '學習計劃', '学習計劃', '学习计划',
+                         '学習計劃', '學習計劃', '学習計劃', '学习计划', '学習計劃', '學習計劃', '学習計劃', '学习计划',
+                         '学習計劃', '學習計劃', '学習計劃', '学习计划', '学習計劃', '學習計劃', '学習計劃', },
+            'Stock': {'股票', '股市', '股票市场', '股票市場', '股市行情'},
+            }
