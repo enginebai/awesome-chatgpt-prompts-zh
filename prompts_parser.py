d73e5ce40c1e81605871ed85d8bb54294d94f887
Engine Bai <enginebai@gmail.com>
2023-05-02 22:35:33 +0800

    Finish prompts classification based on pre-defined category and keywords mapping

--- prompts_parser.py
+++ prompts_parser.py
@@ -5,6 +5,33 @@ import os
 import re
 
 question_dir = 'question'
+output_dir = 'output'
+
+if not os.path.exists(output_dir):
+    os.mkdir(output_dir)
+
+# There are some keywords for each category. We want to classify those keywords and its prompts into the category.
+# C:旅游建议, (4)
+# 	我想去旅游，请问哪个季节去这个地方比较好？
+# 	如果我想去海外旅游，有哪些国家适合3月份旅行？
+# 	我在巴黎待三天，你有什么必去的景点建议吗？
+# 	如果我打算去欧洲旅游，哪些城市是必去的？
+# C:旅游, (6)
+# 	哪些城市是值得一去的著名旅游胜地？
+# 	请介绍一个值得一去的国内旅游景点，包括该景点的特色和相关注意事项。
+# 	去东南亚旅行需要注意哪些安全问题？
+# It belongs to Travel category, but those prompts are not in the same keyword set, we want to classify
+# them into the same category.
+category_keywords = \
+    {'Workout': {'健身', '锻炼'},
+     'Travel': {'旅游', '出行', '出游', '旅途', '旅程', '行程'},
+     'Food': {'美食', '饮食'},
+     'Learning': {'学习', '学習', '學習', '學习'},
+     'Finance': {'股票', '股市', '投資', '投资', '财经', '金融', '理财'},
+     'English': {'英语', '英語', '英文'},
+     'Software': {'计算机', '职场', '職場', '软件', },
+     'AI': {'人工智能', '机器学习', '深度学习'}
+     }
 
 
 def parse_all_question_files():
@@ -13,9 +40,12 @@ def parse_all_question_files():
     print("Parsing files...")
     for f_ in question_files:
         result_map.update(read_file(f_))
+
     result_map = dict(sorted(result_map.items(), key=lambda x: len(x[1])))
-    print_keys(result_map)
-    # print_key_values(result_map)
+    # print_keys(result_map)
+    output_key_values(result_map, output_dir + '/raw.log')
+    classify_map = classify_prompt(result_map, category_keywords)
+    output_key_values(classify_map, output_dir + '/classify.log')
 
 
 def list_question_files():
@@ -53,12 +83,10 @@ def parse_line(line) -> (str, str):
         return category, prompt
 
 
-def print_key_values(map_to_print: dict):
-    with open('category.log', 'w') as f:
+def output_key_values(map_to_print: dict, filename=None):
+    with open(filename, 'w') as f:
         for k, v in map_to_print.items():
-            if len(v) <= 1:
-                continue
-            head = f'C:{k}, ({len(v)})'
+            head = f'Category:{k}, ({len(v)})'
             print(head)
             f.write(head + '\n')
             for terms in v:
@@ -71,19 +99,30 @@ def print_keys(map_to_print: dict):
         print(f'{k} ({len(map_to_print[k])})')
 
 
+def classify_prompt(result_dict: dict, keyword_map: dict) -> dict:
+    """
+    Classify the prompts in different keywords into the same category according to `keyword_map` mapping.
+
+    Args:
+        result_dict: The raw result dictionary that contains keywords and it prompts.
+        keyword_map: The mapping from category to relative keywords.
+
+    Returns:
+        A dictionary with category as key and it relative prompts as value.
+    """
+    category_map = dict()
+    for k, v in result_dict.items():
+        for category, keywords in keyword_map.items():
+            for keyword in keywords:
+                if keyword in k:
+                    if category in category_map:
+                        category_map[category].update(v)
+                    else:
+                        prompts = set()
+                        prompts.update(v)
+                        category_map[category] = prompts
+    return category_map
+
+
 parse_all_question_files()
 # read_file('question/332.md')
-
-# TODO: Add more keywords for each category
-keywords = {'Workout': {'健身', },
-            'Travel': {'旅遊', '旅游', '旅行', '出行', '出游', '出差', '旅途', '旅程', '行程', '行程安排', '行程规划',
-                       '行程计划', '行程安排', '行程规划', '行程计划', '行程安排', '行程规划', '行程计划', '行程安排',
-                       '行程规划', '行程计划', '行程安排', '行程规划', '行程计划', '行程安排', '行程规划', '行程计划',
-                       '行程安排', '行程规划', '行程计划', '行程安排', '行程规划', '行程计划', },
-            'Food': {'美食', },
-            'Learning': {'学习', '学習', '學習', '學习', '学习计划', '学習計劃', '學習計劃', '学習計劃', '学习计划',
-                         '学習計劃', '學習計劃', '学習計劃', '学习计划', '学習計劃', '學習計劃', '学習計劃', '学习计划',
-                         '学習計劃', '學習計劃', '学習計劃', '学习计划', '学習計劃', '學習計劃', '学習計劃', '学习计划',
-                         '学習計劃', '學習計劃', '学習計劃', '学习计划', '学習計劃', '學習計劃', '学習計劃', },
-            'Stock': {'股票', '股市', '股票市场', '股票市場', '股市行情'},
-            }
